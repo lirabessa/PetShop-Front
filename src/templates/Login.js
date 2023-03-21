03848088133aafc1cb2ba45b38dfd315a5b81793
LULUcas0528 <natalirabessa@gmail.com>
2023-03-21 16:14:45 -0300

    Criado tela de Login

--- src/templates/Login.js
+++ src/templates/Login.js
@@ -1,14 +1,48 @@
 import React from 'react';
-import { View , Text} from 'react-native'
+import { View , Text, StyleSheet, TextInput} from 'react-native'
 
 const Login = () => {
+
+    const [number, onChangeNumber] = React.useState('');
     return(
         <>
-            <View>
-                <Text>Tela de Loginziin</Text>
-            </View>
+            <View style = {styles.barra}/>
+            <Text style= {styles.titulo}>Login</Text>
+
+            <TextInput
+                style={styles.input}
+                value={number}
+                placeholder="Nome:"
+                keyboardType="String"
+            />
+
+            <TextInput
+                style={styles.input}
+                value={number}
+                placeholder="Nome:"
+                keyboardType="String"
+            />
+             
         </>
     )
 }
+
+
+
+const styles = StyleSheet.create({
+    titulo:{
+        textAlign:"center", fontSize: 50, fontFamily: "",
+    },
+    barra:{
+        flex:0.15, height:40, backgroundColor: "pink"
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        borderRadius: 10,
+        padding: 10,
+      }
+})
 
 export default Login;
