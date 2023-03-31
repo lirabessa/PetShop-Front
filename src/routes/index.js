0b6d0f656e5cdd4be5c64d840fdf188067f9a3d5
lirabessa <natalirabessa@gmail.com>
2023-03-30 21:55:38 -0300

    Modificado rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -16,7 +16,7 @@ export default function Routes(){
     const Stack = createNativeStackNavigator();
     return(     
             <NavigationContainer>
-                <Stack.Navigator  initialRouteName="Foto">
+                <Stack.Navigator  initialRouteName="TelaHome">
                     <Stack.Screen name = "TelaHome" component= {TelaHome}/>
                     <Stack.Screen name = "CadastroFunc"component= {CadastroFunc}/>
                     <Stack.Screen name="CadastroCli"component={CadastroCli} />
